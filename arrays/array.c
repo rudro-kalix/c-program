c80c8aae937ce01d46dc64bf0951828c53d8ed79
Julkar Nayen Bin Hossain <pubgfukedup@gmail.com>
2026-04-17 23:05:38 +0600

    2026-04-17_23:05

---
+++ arrays/array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (){
+
+     int fst[10] = {12,12,32,34,5334,34,3443};
+
+
+     for( int i =0 ; i <=6; i++){
+
+        printf("%d \n", fst[i]);
+
+     }
+
+
+    return 0 ;
+}
