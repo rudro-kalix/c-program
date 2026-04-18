6ed210f242b409a4322d3bc518e11beb71dfabac
Julkar Nayen Bin Hossain <pubgfukedup@gmail.com>
2026-04-18 19:17:23 +0600

    2026-04-18_19:17

---
+++ arrays/arraysum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+    int sum=0;
+    int arr[10]= {12,12,543,76,45,3,6,5,424,64};
+
+        for( int i=0;i<=9;i++){
+
+            sum=sum+arr[i];
+
+        }
+printf("%d",sum);
+
+
+
+    return 0;
+}
---
+++ arrays/binarysearch.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+
+    int arr[10]= {12,43,65,35,86,56,35,32,24,57};
+    //we will be searching 35 in this case
+
+    for( int i=0; i<=9;i++){
+
+        
+
+    }
+
+
+    return 0;
+}
