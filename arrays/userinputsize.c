e5a5e10fcc28996ccbac886f8dddb330def7e27c
Julkar Nayen Bin Hossain <pubgfukedup@gmail.com>
2026-04-18 20:02:40 +0600

    2026-04-18_20:02

---
+++ arrays/userinputsize.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+
+    int n; 
+    
+    printf("Enter the size of array:");
+    scanf("%d",&n);
+    int arr[n];
+    for (int i =0 ; i <=n-1; i++){
+        printf("Enter array number %d:",i+1);
+        scanf("%d",&arr[i]);
+
+    }
+    for(int j =0 ; j<=n-1;j++){
+
+        printf("%d\n",arr[j]);
+
+    }
+
+
+    return 0;
+}
