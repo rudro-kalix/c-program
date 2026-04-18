3aecffabafb225118e386120581398783c3c3da4
Julkar Nayen Bin Hossain <pubgfukedup@gmail.com>
2026-04-18 14:31:39 +0600

    2026-04-18_14:31

---
+++ arrays/specific_input_output.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main (){
+
+    int arr[5];
+
+    //we will take input of 5 students and then we will print the output of the marks which are less than 33
+
+    for (int i=0; i <=4 ; i++){
+
+        printf("Enter the Marks of student number %d :", i+1);
+        scanf("%d", &arr[i]);
+
+    }
+
+    for( int i=0; i <=5; i ++){
+
+        if (arr[i]>33){
+
+            printf("marks of sudent number %d is :%d ",i+1,arr[i]);
+
+        }
+
+    }
+
+
+    return 0;
+}
